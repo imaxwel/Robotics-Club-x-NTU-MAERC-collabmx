357678492b4957e61530306c69156646b3eb74c6
imaxwel <miscxw@gmail.com>
2026-03-29 10:24:25 +0800

    支持任意路径加载box.urdf

--- Day_1/Some Basic Stuff/Box.py
+++ Day_1/Some Basic Stuff/Box.py
@@ -1,13 +1,18 @@
 import pybullet as p
 import time
 import pybullet_data
+import os
+
+# Get the directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("box.urdf",cubeStartPos, cubeStartOrientation)
+boxId = p.loadURDF(os.path.join(script_dir, "Box.urdf"),cubeStartPos, cubeStartOrientation)
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
