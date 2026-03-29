86ff806827641d80cea20b1e97fe9404e60ffc02
imaxwel <miscxw@gmail.com>
2026-03-29 11:26:19 +0800

    修复任意路径加载urdf

--- Day_1/Some Basic Stuff/2_R_robot.py
+++ Day_1/Some Basic Stuff/2_R_robot.py
@@ -1,13 +1,16 @@
 import pybullet as p
 import time
 import pybullet_data
+import os
+# Get the directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,0)
 planeId = p.loadURDF("plane.urdf")
 StartPos = [0,0,1]
 StartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("2_R_robot.urdf",StartPos, StartOrientation)
+boxId = p.loadURDF(os.path.join(script_dir, "2_R_robot.urdf"),StartPos, StartOrientation)
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
